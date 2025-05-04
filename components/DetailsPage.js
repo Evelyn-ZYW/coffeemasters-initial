2eadfb7d9d159bd60892a951c2e4c5d2e0c8bc9d
Evie Zhao <eviezhao@Evelyns-iMac.local>
2025-05-04 16:24:57 -0700

    clone the template and attach the component to the DOM

--- components/DetailsPage.js
+++ components/DetailsPage.js
@@ -2,6 +2,13 @@ export class DetailsPage extends HTMLElement {
   constructor() {
     super();
   }
+
+  // when the component is attached to the DOM
+  connectedCallback() {
+    const template = document.getElementById("menu-page-template");
+    const content = template.content.cloneNode(true);
+    this.appendChild(content);
+  }
 }
 
 customElements.define("details-page", DetailsPage);
