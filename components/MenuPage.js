4de8106874d0a452913d40e3bf23d247c341a620
Evie Zhao <evelyn.helloworld@gmail.com>
2025-05-05 13:30:18 -0700

    apply CSS in the shadow DOM

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -2,7 +2,19 @@ export class MenuPage extends HTMLElement {
   constructor() {
     super();
 
+    // apply shadow DOM
     this.root = this.attachShadow({ mode: "open" });
+
+    // apply CSS
+    const styles = document.createElement("style");
+    this.root.appendChild(styles);
+
+    async function loadCSS() {
+      const request = await fetch("./components/MenuPage.css");
+      const css = await request.text();
+      styles.textContent = css;
+    }
+    loadCSS();
   }
 
   // when the component is attached to the DOM
